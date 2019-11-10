ab723e582ff43152b859d3bd57c1991ac270e99b
Danielle Thomas <danielle.thomas313@gmail.com>
2019-11-10 14:36:54 -0600

    add more styling and link to repo

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Box, Text } from "grommet";
+import { Box, Text, Anchor } from "grommet";
 import { Star } from "grommet-icons";
 
 const SearchResult = ({ item }) => {
   return (
     <Box direction="row" background="white" pad="medium" justify="between" border={{side: 'bottom', size: '1px', color: 'grey'}} fill>
-      <Box fill>
-        <Text>{item.full_name}</Text>
-        <Text>{item.description}</Text>
+      <Box fill align="start">
+        <Anchor as="a" color="#0366d6" margin={{bottom: 'small'}} label={item.full_name} href={item.html_url} target="_blank"/>
+        <Text color="#586069">{item.description}</Text>
       </Box>
-      <Box fill>{item.language}</Box>
-      <Box fill>{item.stargazers_count}</Box>
+      <Box fill align="center" color="#586069">{item.language}</Box>
+      <Box fill align="center" direction="row" justify="end"><Star size="medium" color="#f1e05a" />{item.stargazers_count}</Box>
     </Box>
   );
 };
